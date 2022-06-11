a1441bb98df28b420f8006178dbd64ecddaab0e6
Sarf1234 <sarfarajshekh69@gmail.com>
2022-06-11 14:34:27 +0530

    first time

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Propspass from './Propspass';
+import "./App.css"
+
+function App(){
+    
+    return(
+        <div className='center'>
+            <Propspass name="sarf raj"  anything="i dont know"/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Propspass.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Propspass = (props) =>{
+    return(
+        <>
+        <h2>Hello my name is {props.name}</h2>
+        <p>this the pass prosps {props.anything}</p>
+        </>
+    )
+}
+
+export default Propspass;
